b7bc286dfac507da4d089e36311b553606843034
kennycallado <kennycallado@hotmail.com>
2023-07-18 12:47:08 +0200

    CLEAN Limpia migraciones

--- src/database/migrations/2023-07-14-180218_create_tokens/up.sql
+++ src/database/migrations/2023-07-14-180218_create_tokens/up.sql
@@ -13,5 +13,4 @@ ALTER TABLE tokens
 --   CHECK (fcm_token IS NOT NULL OR web_token IS NOT NULL);
 
 INSERT INTO tokens VALUES
-  (default, 1, NULL, '{ "endpoint": "https://updates.push.services.mozilla.com/wpush/v2/gAAAAABks_33tHmTlwkBDO7YrhGQveg2-djKwa0ElCGGK4WRnHmPkAjbptJTfnt5yA8wujasgL7LMhmAC2zmkRLi2aaFmNvKH29cKZNKaDWyAfLMwslmT7d9ndXCMTBpldJ3gNyrNom3O_EsOZPsweE_0fd2Q5BCCofh7eTdyHlMYR3fzphiNkk", "expirationTime": null, "keys": { "auth": "ZF2TXINVg_HatgUrq-vMlQ", "p256dh": "BCDSOurLX5C0skbIx0Z4nrlnB1OBFqYOhMMT-jT_Rs0-n2yHHmKG6IMhh8anhSDhAuQeysgOsEbbmQc8pJaTHNA" } }'),
-  (default, 2, NULL, NULL);
+  (default, 1, NULL, '{ "endpoint": "https://updates.push.services.mozilla.com/wpush/v2/gAAAAABks_33tHmTlwkBDO7YrhGQveg2-djKwa0ElCGGK4WRnHmPkAjbptJTfnt5yA8wujasgL7LMhmAC2zmkRLi2aaFmNvKH29cKZNKaDWyAfLMwslmT7d9ndXCMTBpldJ3gNyrNom3O_EsOZPsweE_0fd2Q5BCCofh7eTdyHlMYR3fzphiNkk", "expirationTime": null, "keys": { "auth": "ZF2TXINVg_HatgUrq-vMlQ", "p256dh": "BCDSOurLX5C0skbIx0Z4nrlnB1OBFqYOhMMT-jT_Rs0-n2yHHmKG6IMhh8anhSDhAuQeysgOsEbbmQc8pJaTHNA" } }');
