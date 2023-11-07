d4588a83da49e75f5f79532b64cc63ff829c6054
kennycallado <kennycallado@hotmail.com>
2023-11-07 10:54:19 +0100

    CLEAN

--- src/app/modules/dispatcher/handlers/sender.rs
+++ src/app/modules/dispatcher/handlers/sender.rs
@@ -1,20 +1,17 @@
-use rocket::http::Status;
-
 use base64ct::{Base64UrlUnpadded, Encoding};
+use hyper::{header, Body, Client, StatusCode};
+use hyper_rustls::HttpsConnectorBuilder;
+use rocket::http::Status;
 use web_push_native::{
     jwt_simple::algorithms::ES256KeyPair, p256::PublicKey, Auth, Error, WebPushBuilder,
 };
 
-use hyper::{header, Body, Client, StatusCode};
-use hyper_rustls::HttpsConnectorBuilder;
-
-use crate::app::providers::guards::claims::AccessClaims;
-use crate::app::providers::services::claims::UserInClaims;
-use crate::database::connection::Db;
-
 use crate::app::modules::dispatcher::services::config_getter::ConfigGetter;
 use crate::app::modules::messages::model::NewMessage;
 use crate::app::modules::tokens::model::WebPushToken;
+use crate::app::providers::guards::claims::AccessClaims;
+use crate::app::providers::services::claims::UserInClaims;
+use crate::database::connection::Db;
 
 use crate::app::modules::messages::services::repository as message_repository;
 use crate::app::modules::tokens::services::repository as token_repository;
@@ -62,10 +59,10 @@ pub async fn send_message_id(
 }
 
 pub async fn send_message(
-    db: &Db,
+    _db: &Db,
     _user: UserInClaims,
-    user_id: i32,
-    message: NewMessage,
+    _user_id: i32,
+    _message: NewMessage,
 ) -> Result<Status, Status> {
     // get the token
     // determine if fcm or web push
@@ -84,19 +81,10 @@ async fn to_web_push(token: WebPushToken, message: NewMessage) -> Result<Status,
         }
     };
 
-    // TODO: take care of the unwraps
-    // let message: rocket::serde::json::Value = message.into();
-    // println!("message: {}", message);
-
     let key_pair =
         ES256KeyPair::from_bytes(&Base64UrlUnpadded::decode_vec(api_key.as_str()).unwrap())
             .unwrap();
-    // let builder = WebPushBuilder::new(
-    //     token.endpoint.parse().unwrap(),
-    //     PublicKey::from_sec1_bytes(&Base64UrlUnpadded::decode_vec(&token.p256dh).unwrap()).unwrap(),
-    //     Auth::clone_from_slice(&Base64UrlUnpadded::decode_vec(&token.auth).unwrap()),
-    // ).with_vapid(&key_pair, "mailto:kennycallado@hotmail.com");
-    //
+
     let builder = WebPushBuilder::new(
         token.endpoint.parse().unwrap(),
         PublicKey::from_sec1_bytes(&Base64UrlUnpadded::decode_vec(&token.p256dh).unwrap())
@@ -105,26 +93,19 @@ async fn to_web_push(token: WebPushToken, message: NewMessage) -> Result<Status,
     )
     .with_vapid(&key_pair, "mailto:kennycallado@hotmail.com");
 
-    // Esto no estaba funcionando
-    // let message = rocket::serde::json::Value::from(r#"{ "notification": { "title": "Hello", "body": "World", "icon": "assets/icons/icon-72x72.png", "vibrate": [100, 50, 100], "data": { "primaryKey": 1, "type": "info", "content": ["content"] }, "actions": [{ "action": "explore", "title": "Explore this new world" }] } }"#);
-    // println!("message: {:#?}", message);
-
     let message: rocket::serde::json::Value = message.into();
     let web_push = builder
         .build(message.to_string())
         .unwrap()
         .map(|body| body.into());
 
-    // let blah = r#"{ "notification": { "title": "Hello", "body": "World", "icon": "assets/icons/icon-72x72.png", "vibrate": [100, 50, 100], "data": { "primaryKey": 1, "type": "info", "content": ["content"] }, "actions": [{ "action": "explore", "title": "Explore this new world" }] } }"#;
-    // let web_push = builder.build(blah).unwrap().map(|body| body.into());
-
     let https = HttpsConnectorBuilder::new()
         .with_native_roots()
         .https_only()
         .enable_http1()
         .build();
-    let client: Client<_, Body> = Client::builder().build(https);
 
+    let client: Client<_, Body> = Client::builder().build(https);
     let res = match client.request(web_push).await {
         Ok(response) => Ok(response),
         Err(e) => {
@@ -145,8 +126,8 @@ async fn to_web_push(token: WebPushToken, message: NewMessage) -> Result<Status,
     }
 }
 
-async fn to_fcm(token: &str, message: NewMessage) -> Result<Status, Status> {
-    let api_key = match ConfigGetter::get_fcm_api_key() {
+async fn to_fcm(_token: &str, _message: NewMessage) -> Result<Status, Status> {
+    let _api_key = match ConfigGetter::get_fcm_api_key() {
         Some(api_key) => api_key,
         None => {
             println!("Error: sender; There is no api key for FCM");
